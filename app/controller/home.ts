492bf49f138c844c57ed9a23a57bb169ad90b329
Vic <vic.liu@webcomm.com.tw>
2021-06-05 21:02:54 +0800

    test: 1

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -4,13 +4,8 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
 
-
-    const a=1;
-    console.log(a)
-
-
-
-
+    const a = 1;
+    console.log(a);
 
     ctx.body = await ctx.service.test.sayHi('egg');
   }
